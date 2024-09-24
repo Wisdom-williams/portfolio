d3dc9a3f2c7a7f70bb079c6fcf5e62bcfcfd4eb5
Wisdom-williams <williamswisdommensah@gmail.com>
2024-09-24 23:50:24 +0000

    making updates to the profile

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,43 +1,43 @@
-import { EXPERIENCES } from "../constants"
-import {motion} from "framer-motion"
+import { PROJECTS } from "../constants"
+import{motion} from "framer-motion"
 
-
-const Experience = () => {
+const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
         <motion.h1 
         whileInView={{opacity:1, y:0}}
         initial={{opacity:0, y: -100}}
-        transition={{duration: 1.5}}
-        className="my-20 text-center text-4xl"> Experience</motion.h1>
-    
-        <div>
-            {EXPERIENCES.map((experience, index) => (
+        transition={{duration: 0.5}}
+        className="my-20 text-center text-4xl">Projects</motion.h1>
+      
+      <div>
+      {PROJECTS.map((project, index) => (
                 <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
                     <motion.div 
                     whileInView={{opacity:1, x:0}}
                     initial={{opacity:0, x: -100}}
                     transition={{duration: 1.5}}
                     className="w-full lg:w-1/4">
-                        <p className="mb-2 text-sm text-neutral-400">
-                            {experience.year}
-                        </p>
+                       <img 
+                       src={project.image} 
+                       width={150}
+                       height={150}
+                       alt={project.title} 
+                       className="mb-6 rounded"
+                       />
                     </motion.div>
-                    <motion.div 
+                    <motion.div
                     whileInView={{opacity:1, x:0}}
                     initial={{opacity:0, x: 100}}
                     transition={{duration: 1.5}}
                     className="w-full max-w-xl lg:w-3/4">
                         <h6 className="mb-2 font-semibold">
-                            {experience.role} -{" "}
-                            <span className="text-sm text-purple-100">
-                                {experience.company}
-                            </span>
+                            {project.title}
                         </h6>
                         <p className="mb-2 text-sm text-neutral-400">
-                            {experience.description}
+                            {project.description}
                         </p>
-                        {experience.technologies.map((tech, index) => (
+                        {project.technologies.map((tech, index) => (
                             <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 p-2 px-2 py-1 text-sm
                             font-medium text-purple-800">
                                 {tech}
@@ -48,10 +48,9 @@ const Experience = () => {
             ))
 
             }
-        </div>
+      </div>
     </div>
-
   )
 }
 
-export default Experience
+export default Projects
