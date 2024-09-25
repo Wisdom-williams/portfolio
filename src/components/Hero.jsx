8056479f08775d15c2ca4280858454e8593cc4bb
Wisdom-williams <williamswisdommensah@gmail.com>
2024-09-25 00:09:55 +0000

    making updates to the profile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
                     variants={container(0)}
                     initial="hidden"
                     animate="visible"
-                    classame="pb-16 text-6xl font-thin tracking-tight
+                    className="pb-16 text-6xl font-thin tracking-tight
                     lg:mt-16 lg:text-8xl">Wisdom Williams</motion.h1>
 
                     <motion.span 
